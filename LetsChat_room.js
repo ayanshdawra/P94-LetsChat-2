b2727d456dd985c7a9ede41f0afeed327a32fbd6
ayanshdawra <75238739+ayanshdawra@users.noreply.github.com>
2021-08-27 17:59:21 +0530

    Add files via upload

---
+++ LetsChat_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnXr7Rve2clI7xiXmq_3nBf7f286pPlEs",
+    authDomain: "lets-chat-53fe6.firebaseapp.com",
+    projectId: "lets-chat-53fe6",
+    storageBucket: "lets-chat-53fe6.appspot.com",
+    messagingSenderId: "1002841025926",
+    appId: "1:1002841025926:web:0cc8885052cdfc36091007"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
